be4142c70dd0c77982529daaa2ae328e8eb224b2
Laxmikant Shinde <laxmikant251102@gmail.com>
2025-09-22 23:08:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function dayOfAYear(year){
+	if(year % 4 != 0){
+		return 365;
+	} else if (year % 100 != 0) {
+		return 366;
+	} else if (year % 400 != 0){
+		return 365;
+	} else {
+		return 365;
+	}
+}
